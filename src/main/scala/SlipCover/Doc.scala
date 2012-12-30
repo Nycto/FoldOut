51f0960f80f65ac791a6227701389b28b2029b1e
Nycto <James@RoundEights.com>
2012-12-29 17:11:13 -0800

    Add skeleton Doc

---
+++ src/main/scala/SlipCover/Doc.scala
@@ -0,0 +1,40 @@
+package com.roundeights.foldout
+
+import com.roundeights.scalon.{nObject, nElement, nParser}
+
+/** A CouchDB Document */
+object Doc {
+
+    /** Creates a new document */
+    def apply ( obj: nObject ): Doc = new Doc( obj )
+
+    /** Creates a new document from a JSON string */
+    def apply ( json: String ): Doc = new Doc( nParser.jsonObj(json) )
+
+}
+
+/**
+ * A CouchDB Document
+ *
+ * @param obj The underlying notation object that contains the data for
+ *      this document
+ */
+class Doc ( val obj: nObject ) extends nObject.Interface[Doc] {
+
+    /** Returns this document as JSON */
+    def json: String = obj.json
+
+    /** {@inheritDoc} */
+    override protected def build ( o: nObject ): Doc = new Doc(o)
+
+    /** {@inheritDoc} */
+    override def iterator: Iterator[(String, nElement)] = obj.iterator
+
+    /** {@inheritDoc} */
+    override def get( key: String ): Option[nElement] = obj.get( key )
+
+    /** {@inheritDoc} */
+    override def toMap: Map[String, nElement] = obj.toMap
+
+}
+
