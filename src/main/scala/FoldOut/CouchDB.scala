024295a5c8208900b1c5e1e6a1785fcbf9bc15f5
Nycto <James@RoundEights.com>
2012-12-31 18:37:04 -0800

    Abstract request interface into Requestor

--- src/main/scala/FoldOut/CouchDB.scala
+++ src/main/scala/FoldOut/CouchDB.scala
@@ -1,10 +1,6 @@
 package com.roundeights.foldout
 
-import scala.concurrent.{Promise, Future}
-
-import com.ning.http.client.{
-    AsyncHttpClientConfig, AsyncHttpClient, RequestBuilder
-}
+import scala.concurrent.Future
 
 /**
  * CouchDB companion
@@ -49,50 +45,18 @@ class CouchDB (
     maxConnections: Int = 10
 ) {
 
-    /** Generates URLs for the given parameters */
-    private val url = new UrlBuilder( host, port, ssl )
-
-    /** The HTTP Client for making requests */
-    private val client = new AsyncHttpClient(
-        new AsyncHttpClientConfig.Builder()
-            .setCompressionEnabled(true)
-            .setFollowRedirects(false)
-            .setAllowPoolingConnection(true)
-            .setRequestTimeoutInMs( timeout )
-            .setMaximumConnectionsPerHost( maxConnections )
-            .build()
+    /** The internal interface for making requests to CouchDB */
+    private val requestor = new Requestor(
+        new UrlBuilder( host, port, ssl ),
+        auth, timeout, maxConnections
     )
 
     /** Sends a message to close the connection down */
-    def close: Unit = client.close
-
-    /** Returns a prefilled request builder */
-    private def request(
-        method: String, key: String, params: Map[String, String]
-    ): RequestBuilder = {
-        val builder = new RequestBuilder( method )
-            .setUrl( url.url(key, params) )
-
-        // Add in the authorization header
-        auth.foreach( _.addHeader( builder ) )
-
-        builder
-    }
+    def close: Unit = requestor.close
 
     /** Returns the document with the given key */
-    def get (
-        key: String, params: Map[String, String]
-    ): Future[Option[Doc]] = {
-
-        val promise = Promise[Option[Doc]]()
-
-        client.executeRequest(
-            request("GET", key, params).build(),
-            new Asynchronizer( promise )
-        );
-
-        promise.future
-    }
+    def get ( key: String, params: Map[String, String] ): Future[Option[Doc]]
+        = requestor.get( key, params )
 
     /** Returns the document with the given key */
     def get ( key: String, params: (String, String)* ): Future[Option[Doc]]
---
+++ src/main/scala/FoldOut/Requestor.scala
@@ -0,0 +1,65 @@
+package com.roundeights.foldout
+
+import scala.concurrent.{Promise, Future}
+
+import com.ning.http.client.{
+    AsyncHttpClientConfig, AsyncHttpClient, RequestBuilder
+}
+
+/**
+ * The internal interface for making raw requests to the CouchDB server
+ */
+private[foldout] class Requestor (
+    private val url: UrlBuilder,
+    private val auth: Option[Auth],
+    private val client: AsyncHttpClient
+) {
+
+    /** Alternate constructor that puts together an async client */
+    def this (
+        url: UrlBuilder, auth: Option[Auth],
+        timeout: Int, maxConnections: Int
+    ) = this(
+        url, auth,
+        new AsyncHttpClient(
+            new AsyncHttpClientConfig.Builder()
+                .setCompressionEnabled(true)
+                .setFollowRedirects(false)
+                .setAllowPoolingConnection(true)
+                .setRequestTimeoutInMs( timeout )
+                .setMaximumConnectionsPerHost( maxConnections )
+                .build()
+        )
+    )
+
+    /** Sends a message to close the connection down */
+    def close: Unit = client.close
+
+    /** Returns a prefilled request builder */
+    def buildRequest(
+        method: String, key: String, params: Map[String, String]
+    ): RequestBuilder = {
+        val builder = new RequestBuilder( method )
+            .setUrl( url.url(key, params) )
+
+        // Add in the authorization header
+        auth.foreach( _.addHeader( builder ) )
+
+        builder
+    }
+
+    /** Executes the given request and returns a promise */
+    def execute ( request: RequestBuilder ): Future[Option[Doc]] = {
+        val promise = Promise[Option[Doc]]()
+        client.executeRequest( request.build(), new Asynchronizer( promise ) );
+        promise.future
+    }
+
+    /** Returns the document with the given key */
+    def get ( key: String, params: Map[String, String] ): Future[Option[Doc]]
+        = execute( buildRequest("GET", key, params) )
+
+}
+
+
+
